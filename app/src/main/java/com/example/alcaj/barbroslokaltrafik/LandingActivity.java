7813129c53d4bdb3ebc6f0f84fc7772bc903d4de
Flaxen <alexander.carlsson@live.se>
2017-06-13 13:49:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alcaj/barbroslokaltrafik/LandingActivity.java
@@ -0,0 +1,14 @@
+
+package com.example.alcaj.barbroslokaltrafik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
