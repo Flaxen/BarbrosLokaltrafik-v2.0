84bebeff98d6b3b695a9854a9558654070655ca2
Flaxen <alexander.carlsson@live.se>
2017-06-14 13:04:46 +0200

    Added scrollview of station images to main activity

--- app/src/main/java/com/example/alcaj/barbroslokaltrafik/MainActivity.java
+++ app/src/main/java/com/example/alcaj/barbroslokaltrafik/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.alcaj.barbroslokaltrafik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
